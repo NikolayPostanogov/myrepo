4183583a217b469e4e6081bc2e3e21f5db1cbd69
NikolayPostanogov <postanogov@nwtn-group.com>
2021-03-22 19:34:24 +0500

    Create text.php

---
+++ test_docker/www/hw/text.php
@@ -0,0 +1,23 @@
+<?php
+echo "Год "; 
+$year=$_GET['Year'];
+echo $year;
+echo " високосный?";
+$text="";
+if (is_numeric($year))
+{
+    if ($year % 400 == 0 || ($year % 100 != 0 && $year % 4 == 0))
+    {
+        $text = "ДА";
+    }
+    else
+    {
+        $text = "НЕТ";
+    }
+}
+else
+{
+    $text = "ОШИБКА ВО ВХОДНЫХ ДАННЫХ";
+}
+echo " $text";
+?> 
