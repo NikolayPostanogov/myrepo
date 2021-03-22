0cbcee97d003e9b9e78ff90371cde0262e9bb785
NikolayPostanogov <postanogov@nwtn-group.com>
2021-03-23 01:52:22 +0500

    Create text.php

---
+++ hw3/www/hw/text.php
@@ -0,0 +1,23 @@
+<?php
+$hips=$_GET['hips'];
+$smus=$_GET['smus'];
+
+#n - количество хипстеров, m - количество смузи
+function foo($n, $m){
+	if (is_numeric($m) and is_numeric($n)){
+    	$kol= (int)($m / $n);
+    }
+    else{
+    	$kol="Неверные входные данные";
+    }
+    return $kol;
+}
+
+$kol = foo($hips, $smus);
+if(is_numeric($kol)){
+	echo "Количество смузи доставшееся каждому хипстеру: $kol";
+}
+else{
+	echo "$kol";
+}
+?> 
