1775a8419136aceecb4ebc0e5adaa45398f283ff
NikolayPostanogov <postanogov@nwtn-group.com>
2021-03-24 23:57:07 +0500

    Update index.php

--- hw2/www/hw/index.php
+++ hw2/www/hw/index.php
@@ -8,6 +8,38 @@
 
 <BODY>
 
-</BODY>
-</HTML>
+<?php
 
+$array = array("abcd", "bar", "abcde", "abcdef", "abc", "abasd");
+$pref = "abcd";
+
+#Функция для вывода массива на экран.
+function printing($array){
+	foreach ($array as &$value) {
+    echo "$value; ";
+	}
+}
+
+#Функция, возвращает массив значений элементов массива $array, которые начинаются с префикса $pref
+function foo($pref, $array){
+	foreach ($array as &$value) {
+		$arr=array();
+	    $rest = substr($value, 0, strlen($pref));
+	    if($rest === $pref){
+	        array_push($arr , $value);
+	    }
+	    return $arr;
+	}
+}
+
+echo "Массив: ";
+printing($array);
+echo "Префикс: $pref; ";
+echo "Результат: ";
+printing(foo($pref, $array));
+
+?>
+
+</BODY>
+
+</HTML>
